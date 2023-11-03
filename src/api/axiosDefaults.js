9116fc4c6d611d99cbe409d714d934e3711df13c
Mushbt <mustafa_habet@hotmail.com>
2023-11-03 18:01:05 +0000

    Commit

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://task-manager-hbt-8627598f1957.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
